5d7e24ab8076d2588ed85e8441644735572abbbc
Ainul Yaqin <myaqien27@gmail.com>
2025-05-24 10:16:14 +0700

    merubah icon

--- lib/main.dart
+++ lib/main.dart
@@ -115,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.minimize),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
